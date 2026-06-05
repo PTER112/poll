34b56df36128b3b95bcd15e37e527ab8ac17fa59
chuji112 <3639037759@qq.com>
2026-06-05 20:54:41 +0800

    最初的版本

---
+++ poll-pojo/src/test/java/org/yjx/pollpojo/PollPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yjx.pollpojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
